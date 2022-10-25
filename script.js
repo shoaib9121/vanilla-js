6ac3e0012ddc5ec2a85a06918cd335d0a9b0420f
Shoaib Ahmad <shoaib9121@yahoo.com>
2022-10-25 15:14:31 +0500

    log updates

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function itemsCollection() {
   this.items = [];
-
   this.add = function (name) {
     if (this.items.indexOf(name) === -1) {
       this.items.push(name);
@@ -22,13 +21,11 @@ function itemsBehaviors() {}
 itemsBehaviors.prototype = new itemsCollection();
 
 var itemsInstance = new itemsBehaviors();
-
 console.log(itemsInstance.items);
-console.log(itemsInstance.add("shoaib"));
-console.log(itemsInstance.add("temp"));
-
+itemsInstance.add("shoaib");
+itemsInstance.add("temp");
 console.log(itemsInstance.getNames());
-console.log(itemsInstance.add("ahmad"));
+itemsInstance.add("ahmad");
 console.log(itemsInstance.getNames());
-console.log(itemsInstance.remove("temp"));
+itemsInstance.remove("temp");
 console.log(itemsInstance.getNames());
