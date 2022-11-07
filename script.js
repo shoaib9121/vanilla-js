54fe8c16814b39b7204266bfa8dd12357571b28b
Shoaib Ahmad <shoaibahmad@NIS-PK619s-MacBook-Pro.local>
2022-11-07 17:18:08 +0500

    without constructor approach

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/**
+ * With Constructor approach
+ */
 function itemsCollection() {
   this.items = [];
   this.add = function (name) {
@@ -19,8 +22,44 @@ function itemsCollection() {
 }
 function itemsBehaviors() {}
 itemsBehaviors.prototype = new itemsCollection();
-
 var itemsInstance = new itemsBehaviors();
+console.log(itemsInstance.items);
+itemsInstance.add("shoaib");
+itemsInstance.add("temp");
+console.log(itemsInstance.getNames());
+itemsInstance.add("ahmad");
+console.log(itemsInstance.getNames());
+itemsInstance.remove("temp");
+console.log(itemsInstance.getNames());
+console.log("==========================");
+
+/**
+ * Without Constructor approach
+ * @returns
+ */
+function itemsCollection() {
+  let items = [];
+  let obj = {};
+  obj.add = function (name) {
+    if (items.indexOf(name) === -1) {
+      items.push(name);
+    }
+  };
+
+  obj.remove = function (name) {
+    let index = items.indexOf(name);
+    if (index) {
+      items.splice(index, 1);
+    }
+  };
+
+  obj.getNames = function () {
+    return items;
+  };
+
+  return obj;
+}
+var itemsInstance = itemsCollection();
 console.log(itemsInstance.items);
 itemsInstance.add("shoaib");
 itemsInstance.add("temp");
