a4e41e2aa5a1a689724f61ce29ca5b302ca2107e
Shoaib Ahmad <sahmad@nisum.com>
2023-07-06 20:10:52 +0500

    comments added

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/**
+ * Add and multiply
+ * @param  {...any} funcs
+ * @returns
+ */
 function compose(...funcs) {
   if (funcs.length < 2) return funcs[0];
 
@@ -9,16 +14,24 @@ function compose(...funcs) {
     };
   });
 }
-
 const add5 = (x) => x + 5;
 const multiply = (x, y = 2) => x * y;
 // const multiplyAndAdd5 = compose(add5);
 // const multiplyAndAdd5 = compose(add5, multiply);
 // console.log(multiplyAndAdd5(3, 2));
 
+/**
+ * Divde, square and get max
+ * @param {*} a
+ * @returns
+ */
 const divideBy2 = (a) => a / 2;
 const square = (a) => a * a;
 const max = (...args) => Math.max.apply(null, args);
 
 const multiplyAndAdd5 = compose(divideBy2, square, max);
 console.log(multiplyAndAdd5(2, 1, 4, 3));
+
+/**
+ * Flatten and concat given array of arrays
+ */
